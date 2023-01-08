3e96a9ba2fd45e9d6b0413af5ce909e1b855770c
Mateus Almeida <Matt.gyn@hotmail.com>
2023-01-08 14:19:54 -0300

    Verifica se deu velha

--- src/JogoDaVelha.java
+++ src/JogoDaVelha.java
@@ -14,6 +14,10 @@ public class JogoDaVelha {
         while(game == true){
             desenhaJogo(velha);
             vitoria = verificaVitoria(velha);
+            if(deuVelha(velha) == true){
+                System.out.println("DEU VELHA");
+                break;
+            }
             if(vitoria != ' '){
                 System.out.printf("Jogador %s venceu%n", vitoria);
                 break;
@@ -52,7 +56,7 @@ public class JogoDaVelha {
 
     public static int[] jogar(Scanner scan, char sa){
         int p[] = new int[2];
-        System.out.printf("%s %c%n","Quem joga: ",sa);
+        System.out.printf("%n%s %c%n%n","Quem joga: ",sa);
         System.out.print("Informe a linha: ");
         p[0] = scan.nextInt();
         System.out.print("Informe a coluna: ");
@@ -77,10 +81,28 @@ public class JogoDaVelha {
         }
     }
 
+    public static Boolean deuVelha(Campo[][] velha){
+        int dv = 0;
+        Boolean result;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(velha[i][j].getSimbolo() != ' '){
+                    dv++;
+                }
+            }
+        }
+        if(dv == 9){
+            result = true;
+        }else {
+            result = false;
+        }
+
+        return result;
+    }
+
     public static char verificaVitoria(Campo[][] velha){
         char winner=' ';
         
-        /* */
         if(velha[0][0].getSimbolo() == velha[0][1].getSimbolo() && velha[0][1].getSimbolo() == velha[0][2].getSimbolo()){
             winner = velha[0][0].getSimbolo();
         }else {
@@ -114,6 +136,7 @@ public class JogoDaVelha {
                 }
             }
         }
+
         return winner;
     }
     
